8b934b27509b4c21d206fde1c06a426372091f66
Federico-Maresca <62501138+Federico-Maresca@users.noreply.github.com>
2021-01-05 15:04:17 +0100

    Update alexnet.py

--- alexnet.py
+++ alexnet.py
@@ -11,7 +11,7 @@ model_urls = {
     'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
 }
 
-
+# testmodified
 class AlexNet(nn.Module):
 
     def __init__(self, num_classes: int = 1000) -> None:
